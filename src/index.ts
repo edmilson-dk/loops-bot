af0e21bc20012e2eb9079eeabd9caccb95d47177
edmilson-dk <droidvidaboa@gmail.com>
2021-09-21 10:27:26 -0300

    feat: added join channel validations

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,25 @@ client.on("message", (message) => {
   const { args, command } = getBotCommandArgs(message.content || "");
   const isValid = isValidCommand(command);
 
-  if (message?.author?.bot) {
+  if (message.author?.bot) {
     return;
   }
 
-  if (message && !message?.author?.bot) {
-    if (!isValid) {
-      message.channel?.send("Invalid command, use !loop or !stop command");
-      return;
-    }
+  if (!isValid) {
+    message.channel?.send("Invalid command, use !loop or !stop command");
+    return;
+  }
+
+  if (args.length === 0) {
+    message.channel?.send("You need to provide a music link");
+    return;
+  }
+
+  if (!message.member?.voice.channel) {
+    message.channel?.send("You need to be in a voice channel to use this command!");
+    return;
+  } else {
+    message.member?.voice.channel?.join();
   }
 });
 
