242fc118160f7d7ff776f75a184d8ecdb95f1e8d
edmilson-dk <droidvidaboa@gmail.com>
2021-09-21 10:14:52 -0300

    feat: added command validation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { Client } from "discord.js";
 dotenv.config();
 
-import { getBotCommandArgs } from "./helpers/parserCommands";
+import { getBotCommandArgs, isValidCommand } from "./helpers/parserCommands";
 
 const TOKEN = process.env.BOT_SECRET_TOKEN;
 
@@ -13,8 +13,19 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-  console.log(getBotCommandArgs(message.content as string));
-  console.log(message.content);
+  const { args, command } = getBotCommandArgs(message.content || "");
+  const isValid = isValidCommand(command);
+
+  if (message?.author?.bot) {
+    return;
+  }
+
+  if (message && !message?.author?.bot) {
+    if (!isValid) {
+      message.channel?.send("Invalid command, use !loop or !stop command");
+      return;
+    }
+  }
 });
 
 client.login(TOKEN);
