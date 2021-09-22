0ee96b95aefd8ac5e8c5af58bba4eb27e771f837
edmilson-dk <droidvidaboa@gmail.com>
2021-09-22 11:57:02 -0300

    feat: added news musics

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,9 @@
 export const COMMANDS_NAMES = ["!loop", "!stop"];
 export const MUSICS = [
+  "https://www.youtube.com/watch?v=51u5fnyrGj4",
   "https://www.youtube.com/watch?v=zecspHTfwjo",
-  "https://www.youtube.com/watch?v=GcVF3Z6KT9s",
   "https://www.youtube.com/watch?v=uFvenQqzk-8",
   "https://www.youtube.com/watch?v=jLfWZ0nhTAE",
-  // "https://www.youtube.com/watch?v=5jbpMeoO0f8",
-  // "https://www.youtube.com/watch?v=5JCIHeQsUog",
+  "https://www.youtube.com/watch?v=5vub-_NWj2o",
+  "https://www.youtube.com/watch?v=aoH4kc_gsGo",
 ];
