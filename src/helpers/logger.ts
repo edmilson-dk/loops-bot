ffb658489fefdd6e369fb11655fb9d5cf93273db
edmilson-dk <droidvidaboa@gmail.com>
2021-09-26 19:05:05 -0300

    feat: created new logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,11 +1,26 @@
-import pino from "pino";
+const date = new Date();
 
-export const logger = pino(
-  {
-    level: process.env.LOG_LEVEL || "info",
-    prettyPrint: {
-      colorize: true,
-    },
-  },
-  process.stdout,
-);
+class Logger {
+  private readonly day: number = date.getDate();
+  private readonly month: number = date.getMonth() + 1;
+  private readonly year: number = date.getFullYear();
+  private readonly minutes: number = date.getMinutes();
+  private readonly hours: number = date.getHours();
+  private readonly seconds: number = date.getSeconds();
+
+  private readonly timestamp: string = `${this.day}-${this.month}-${this.year} [${this.hours}:${this.minutes}:${this.seconds}]`;
+
+  public info(message: string): void {
+    console.log(`${this.timestamp} [INFO] ${message}`);
+  }
+
+  public error(message: string): void {
+    console.log(`${this.timestamp} [ERROR] ${message}`);
+  }
+
+  public warn(message: string): void {
+    console.log(`${this.timestamp} [WARN] ${message}`);
+  }
+}
+
+export const logger = new Logger();
