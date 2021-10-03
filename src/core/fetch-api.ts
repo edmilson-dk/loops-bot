7947987b3f20cb59e346b57d0eaa04ffae5d3d89
edmilson-dk <droidvidaboa@gmail.com>
2021-10-03 20:07:13 -0300

    feat: added select http agente

--- src/core/fetch-api.ts
+++ src/core/fetch-api.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import fs from "fs";
 import https from "https";
+import http from "http";
 
 import { logger } from "../helpers/logger";
 import { MusicInfoType } from "../types";
 
 export class FetchApi {
   private readonly baseUrl: string = process.env.BASE_URL_API as string;
+  private readonly httpsOrHttp = process.env.NODE_ENV === "production" ? https : http;
+
   private readonly API = axios.create({
     baseURL: this.baseUrl,
   });
@@ -19,7 +22,7 @@ export class FetchApi {
   public downloadMusic(id: string, saveFrom: string, saveName: string | number): string {
     const writeFileStream = fs.createWriteStream(`${saveFrom}/${saveName}.mp3`);
 
-    const request = https.get(`${this.baseUrl}/musics/download/${id}`, (response) => {
+    const request = this.httpsOrHttp.get(`${this.baseUrl}/musics/download/${id}`, (response) => {
       response.pipe(writeFileStream);
       response.on("end", () => {
         logger.info(`${saveName} downloaded`);
