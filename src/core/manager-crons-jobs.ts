dc6545ac26f584e6b2aa95a47bc8f4a003e21f95
edmilson-dk <droidvidaboa@gmail.com>
2021-10-07 12:45:50 -0300

    feat: added start job every 1 hour

--- src/core/manager-crons-jobs.ts
+++ src/core/manager-crons-jobs.ts
@@ -6,10 +6,10 @@ export class ManagerCronsJobs {
   private readonly fetchApi: FetchApi = new FetchApi();
 
   public startUpdatedMusicsJob(): void {
-    // job start every 23 hours
+    // job start every 1 hour
 
     const job = new CronJob(
-      "0 0 23 * * * ",
+      "0 */1 * * *",
       async () => {
         await this.fetchApi.emitUpdateMusicList();
       },
