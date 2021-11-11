658791c87ac2cc8d30659babeee6da0232b6fdac
edmilson-dk <droidvidaboa@gmail.com>
2021-11-11 10:14:33 -0300

    feat: added logger colors

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,25 +1,31 @@
+import chalk from "chalk";
+
 const date = new Date();
 
 class Logger {
   private readonly day: number = date.getDate();
   private readonly month: number = date.getMonth() + 1;
   private readonly year: number = date.getFullYear();
-  private readonly minutes: number = date.getMinutes();
-  private readonly hours: number = date.getHours();
-  private readonly seconds: number = date.getSeconds();
+  private readonly minutes: number | string =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  private readonly hours: number | string =
+    date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  private readonly seconds: number | string =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+  private readonly pmOrAm: string = this.hours >= 12 ? "PM" : "AM";
 
-  private readonly timestamp: string = `${this.day}-${this.month}-${this.year} [${this.hours}:${this.minutes}:${this.seconds}]`;
+  private readonly timestamp: string = `${this.day}-${this.month}-${this.year} [${this.hours}h:${this.minutes}m:${this.seconds}s - ${this.pmOrAm}]`;
 
   public info(message: string): void {
-    console.log(`${this.timestamp} [INFO] ${message}`);
+    console.log(chalk.cyan(`${this.timestamp} [INFO] - ${message}`));
   }
 
   public error(message: string): void {
-    console.log(`${this.timestamp} [ERROR] ${message}`);
+    console.log(chalk.red(`${this.timestamp} [ERROR] - ${message}`));
   }
 
   public warn(message: string): void {
-    console.log(`${this.timestamp} [WARN] ${message}`);
+    console.log(chalk.yellow(`${this.timestamp} [WARN] - ${message}`));
   }
 }
 
